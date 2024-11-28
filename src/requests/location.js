4ade936f974cd1e9fa52f54e4648a7ae64f80586
daniyal8 <daniyalsadiq4@gmail.com>
2024-11-28 07:19:43 +0500

    Update: location Request

--- src/requests/location.js
+++ src/requests/location.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_LOCATION_API_KEY;
-const RAPID_API_HOST = import.meta.env.VITE_LOCATION_HOST
 
 const location = async (cityName, countryCode) => {
     try {
         const response = await axios.get(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${cityName}&limit=10`, {
             headers: {
                 'x-rapidapi-key': API_KEY,
-                'x-rapidapi-host': RAPID_API_HOST
+                'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
             },
         });
         return response.data;
